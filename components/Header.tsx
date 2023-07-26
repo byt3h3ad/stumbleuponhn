5fce1ae87786e69e8a51aebfcc9760e40627ddac
byt3h3ad <adhirajd007@gmail.com>
2023-07-26 13:10:40 +0530

    fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,13 +10,13 @@ export default function Header() {
     <header className="sticky top-0 z-40 w-full border-b border-white/[0.1] bg-zinc-900 py-4">
       <div className="flex items-center justify-center">
         <div className="flex gap-6">
-          <div
+          <button
             onClick={() => router.refresh()}
             className="flex items-center space-x-2"
           >
             <Image src={logo} alt={logo} className="h-6 w-6" />
             <span className="inline-block font-bold text-xl">Hacker News</span>
-          </div>
+          </button>
         </div>
       </div>
     </header>
