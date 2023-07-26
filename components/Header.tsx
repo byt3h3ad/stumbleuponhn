da2464a18b7ba56446fb3b68b1d01478a0d902b2
byt3h3ad <adhirajd007@gmail.com>
2023-07-26 13:01:03 +0530

    use router.refresh()

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import logo from "@/public/next.svg";
 
 export default function Header() {
@@ -8,7 +8,10 @@ export default function Header() {
     <header className="sticky top-0 z-40 w-full border-b border-white/[0.1] bg-zinc-900 py-4">
       <div className="flex items-center justify-center">
         <div className="flex gap-6">
-          <div onClick={() => router.reload()} className="flex items-center space-x-2">
+          <div
+            onClick={() => router.refresh()}
+            className="flex items-center space-x-2"
+          >
             <Image src={logo} alt={logo} className="h-6 w-6" />
             <span className="inline-block font-bold text-xl">Hacker News</span>
           </div>
