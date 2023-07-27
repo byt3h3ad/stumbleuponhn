682f08b3959f6965321a211d011c57a4e3bd4368
byt3h3ad <adhirajd007@gmail.com>
2023-07-27 12:10:19 +0530

    fix url undefined bug

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,10 @@
-import { item, getData } from "@/utils";
+import { item, getData } from "@/lib";
 import Link from "next/link";
 
 export default async function Card() {
   const data: item = await getData();
+  const HNPostLink: string = `https://news.ycombinator.com/item?id=${data.id}`;
+  data.url ? data.url : HNPostLink; // sometimes there is no link and only text is present
   return (
     <section className="flex flex-col gap-2 p-2">
       <Link
@@ -15,7 +17,7 @@ export default async function Card() {
       </Link>
       <p className="text-lg text-center">
         <Link
-          href={`https://news.ycombinator.com/item?id=${data?.id}`}
+          href={HNPostLink}
           title={data?.title}
           target="_blank"
           rel="noopener noreferrer"
